34f522ec4b49f9edd73f75d06ccb60c25028a954
Dovlat <dovlat93@gmail.com>
2022-12-27 12:46:52 +0100

    adds dark theme to storybook

--- apps/storybook/.storybook/preview.cjs
+++ apps/storybook/.storybook/preview.cjs
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 
+import { themes } from '@storybook/theming';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  darkMode: {
+    current: 'dark',
+    dark: { ...themes.dark, appBg: 'black' },
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
